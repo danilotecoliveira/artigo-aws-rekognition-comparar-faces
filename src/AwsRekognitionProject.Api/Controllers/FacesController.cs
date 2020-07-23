1d5e146abd136197ecfb1e64dda69a3ccbcfdd86
Danilo Oliveira <danilotecoliveira@gmail.com>
2020-07-23 11:50:59 -0300

    Juntando as controllers

--- src/AwsRekognitionProject.Api/Controllers/FacesController.cs
+++ src/AwsRekognitionProject.Api/Controllers/FacesController.cs
@@ -2,23 +2,27 @@ using System;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AwsRekognitionProject.Api.Services;
+using AwsRekognitionProject.Api.Domain;
 using AwsRekognitionProject.Api.Entities;
 
 namespace AwsRekognitionProject.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CompareFacesController : ControllerBase
+    public class FacesController : ControllerBase
     {
+        private readonly IServiceDetectFaces _serviceDetectFaces;
         private readonly IServiceCompareFaces _serviceCompareFaces;
 
-        public CompareFacesController(IServiceCompareFaces serviceCompareFaces)
+        public FacesController(
+            IServiceDetectFaces serviceDetectFaces,
+            IServiceCompareFaces serviceCompareFaces)
         {
+            _serviceDetectFaces = serviceDetectFaces;
             _serviceCompareFaces = serviceCompareFaces;
         }
 
-        [HttpGet]
+        [HttpGet("facematch")]
         public async Task<IActionResult> GetFaceMatches([FromBody] FaceMatchRequest faceMatchRequest)
         {
             try
@@ -29,6 +33,23 @@ namespace AwsRekognitionProject.Api.Controllers
                 );
 
                 return StatusCode(HttpStatusCode.OK.GetHashCode(), result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+
+        [HttpGet("findfaces")]
+        public async Task<IActionResult> GetFaceMatches([FromBody] FindFacesRequest request)
+        {
+            try
+            {
+                var response = await _serviceDetectFaces.DetectFacesAsync(
+                    request.SourceImage
+                );
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), response);
             }
             catch (Exception ex)
             {
