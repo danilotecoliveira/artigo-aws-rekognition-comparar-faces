f3e507f789e4bbef94cf032a5f32fd5edd0c34a5
Danilo Oliveira <danilotecoliveira@gmail.com>
2020-07-22 23:40:42 -0300

    Criando a controller detectFaces

---
+++ src/AwsRekognitionFaceCompare.Api/Controllers/DetectFacesController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwsRekognitionFaceCompare.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DetectFacesController : ControllerBase
+    {
+        
+    }
+}
